37b275bbdbaa0f5d774026e4988b7efc7e3c09b9
koki-develop <kou.pg.0131@gmail.com>
2023-04-19 05:33:18 +0900

    pass time

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"os"
+	"strconv"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -10,10 +11,20 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use: "sheep",
+	Use:          "sheep [time]",
+	Short:        "Sleep with Sheep",
+	Long:         "Sleep with Sheep.",
+	Args:         cobra.ExactArgs(1),
+	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		t, err := strconv.ParseFloat(args[0], 64)
+		if err != nil {
+			return err
+		}
+
+		base := time.Second
 		m := model.New(&model.Config{
-			Duration: time.Second * 5,
+			Duration: time.Duration(t * float64(base)),
 		})
 		p := tea.NewProgram(m)
 		if _, err := p.Run(); err != nil {
