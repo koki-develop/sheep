e074932916aa721791473b7ef30df74ffdb525ce
koki-develop <kou.pg.0131@gmail.com>
2023-04-18 21:44:03 +0900

    add rootCmd

---
+++ cmd/root.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "sheep",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
