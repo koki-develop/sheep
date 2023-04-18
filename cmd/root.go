8daf9fa16482aacf96bb2151c4fe9d355fdb5142
koki-develop <kou.pg.0131@gmail.com>
2023-04-18 21:53:57 +0900

    implement sleep

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/koki-develop/sheep/internal/model"
@@ -11,12 +12,14 @@ import (
 var rootCmd = &cobra.Command{
 	Use: "sheep",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		m := model.New()
+		m := model.New(&model.Config{
+			Duration: time.Second * 2,
+		})
 		p := tea.NewProgram(m)
 		if _, err := p.Run(); err != nil {
 			return err
 		}
-		if m.Abort {
+		if m.Aborted() {
 			os.Exit(130)
 		}
 
--- internal/model/model.go
+++ internal/model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"time"
+
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -13,37 +15,59 @@ type keymap struct {
 	Abort key.Binding
 }
 
-type Model struct {
-	Abort  bool
-	keymap keymap
+type Config struct {
+	Duration time.Duration
 }
 
-func New() *Model {
+type Model struct {
+	aborted  bool
+	duration time.Duration
+	keymap   keymap
+}
+
+func New(cfg *Config) *Model {
 	return &Model{
-		Abort: false,
+		aborted:  false,
+		duration: cfg.Duration,
 		keymap: keymap{
 			Abort: key.NewBinding(key.WithKeys(tea.KeyCtrlC.String())),
 		},
 	}
 }
 
+func (m *Model) Aborted() bool {
+	return m.aborted
+}
+
 func (m *Model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(
+		m.sleep(),
+	)
 }
 
 func (m *Model) View() string {
 	return "sheep"
 }
 
+type sleptMsg struct{}
+
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case sleptMsg:
+		return m, tea.Quit
 	case tea.KeyMsg:
 		switch {
 		case key.Matches(msg, m.keymap.Abort):
-			m.Abort = true
+			m.aborted = true
 			return m, tea.Quit
 		}
 	}
 
 	return m, nil
+}
+
+func (m *Model) sleep() tea.Cmd {
+	return tea.Tick(m.duration, func(t time.Time) tea.Msg {
+		return sleptMsg{}
+	})
 }
