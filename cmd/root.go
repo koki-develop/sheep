7254180c3456f49793ec2e70bdfbdf183a20989f
koki-develop <kou.pg.0131@gmail.com>
2023-04-19 05:28:28 +0900

    render sheep

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ var rootCmd = &cobra.Command{
 	Use: "sheep",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		m := model.New(&model.Config{
-			Duration: time.Second * 2,
+			Duration: time.Second * 5,
 		})
 		p := tea.NewProgram(m)
 		if _, err := p.Run(); err != nil {
--- internal/model/model.go
+++ internal/model/model.go
@@ -20,18 +20,26 @@ type Config struct {
 }
 
 type Model struct {
-	aborted  bool
-	duration time.Duration
-	keymap   keymap
+	aborted     bool
+	duration    time.Duration
+	windowWidth int
+	keymap      keymap
+	frame       int
+	sheep       sheep
+	awake       bool
 }
 
 func New(cfg *Config) *Model {
 	return &Model{
-		aborted:  false,
-		duration: cfg.Duration,
+		aborted:     false,
+		duration:    cfg.Duration,
+		windowWidth: 0,
 		keymap: keymap{
 			Abort: key.NewBinding(key.WithKeys(tea.KeyCtrlC.String())),
 		},
+		frame: 0,
+		sheep: simpleSheep,
+		awake: false,
 	}
 }
 
@@ -42,19 +50,31 @@ func (m *Model) Aborted() bool {
 func (m *Model) Init() tea.Cmd {
 	return tea.Batch(
 		m.sleep(),
+		m.tick(),
 	)
 }
 
 func (m *Model) View() string {
-	return "sheep"
+	if m.awake {
+		return m.sheep.Awake
+	} else {
+		return m.sheep.Asleep[m.frame]
+	}
 }
 
 type sleptMsg struct{}
+type tickMsg struct{}
 
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.windowWidth = msg.Width
 	case sleptMsg:
+		m.awake = true
 		return m, tea.Quit
+	case tickMsg:
+		m.frame = (m.frame + 1) % len(m.sheep.Asleep)
+		return m, m.tick()
 	case tea.KeyMsg:
 		switch {
 		case key.Matches(msg, m.keymap.Abort):
@@ -69,5 +89,11 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m *Model) sleep() tea.Cmd {
 	return tea.Tick(m.duration, func(t time.Time) tea.Msg {
 		return sleptMsg{}
+	})
+}
+
+func (m *Model) tick() tea.Cmd {
+	return tea.Tick(500*time.Millisecond, func(t time.Time) tea.Msg {
+		return tickMsg{}
 	})
 }
